d3af0abc57e4541f0c8125fa5bba267c3b3c5be2
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-21 12:18:22 -0300

    protótipo

--- Aula 4/exercicios/exercicio 4/mian.c
+++ Aula 4/exercicios/exercicio 4/mian.c
@@ -9,6 +9,44 @@ int main(){
 	pid[0] = fork();
 
 	if(pid[0] < 0){
-		
+		printf("Erro ao criar processo \n");
+        return 1;
+	}
+	if(pid[0] > 0){
+		pid[1] = fork();
+
+		if(pid[1] < 0){
+			printf("Erro ao criar processo \n");
+			return 1;
+		}
+	}
+
+	if((pid[0] > 0) && (pid[1] > 0)){
+		for(int i=1; i<=50; ++i){
+			printf("PID=%d; PPID=%d; %d\n", getpid(), getppid(), i);
+			sleep(2);
+		}
+		printf("Processo pai vai morrer");
+		exit(-1);
+	}
+
+	if(pid[0]==0){
+		printf("Filho 1 foi criado");
+		for(int i=100; i<=199; ++i){
+			printf("PID=%d; PPID=%d; %d\n", getpid(), getppid(), i);
+			sleep(1);
+		}
+		printf("Filho 1 vai morrer");
+		exit(-1);
+	}
+
+	if(pid[1]==0){
+		printf("Filho 2 foi criado");
+		for(int i=200; i<=299; ++i){
+			printf("PID=%d; PPID=%d; %d\n", getpid(), getppid(), i);
+			sleep(1);
+		}
+		printf("Filho 2 vai morrer");
+		exit(-1);
 	}
 }
